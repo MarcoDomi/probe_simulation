2f18cd44f6e7585fc36f2b72876837fb48b0b0b5
Marco Dominguez <47532404+MarcoDomi@users.noreply.github.com>
2024-12-01 13:40:12 -0600

    create the grid that represents the galaxy

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
 
-let galaxy = document.querySelector("#galaxy");
+const galaxyDimension = 80;
+let galaxy = document.querySelector("div");
+
+function createRow() {
+    let row = document.createElement("div");
+    row.classList = "row";
+
+    for (let i = 0; i < galaxyDimension; i++){
+        let item = document.createElement("div");
+        item.classList = "item";
+        row.appendChild(item);
+    }
+
+    return row;
+}
+
+function createGalaxy() {
+    for (let i = 0; i < galaxyDimension; i++){
+        galaxy.appendChild(createRow());
+    }
+}
+
+createGalaxy();
+
+let c = document.querySelectorAll('.item');
+
+let specialDiv = c[4000];
+specialDiv.style.backgroundColor = "red";
