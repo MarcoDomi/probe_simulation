1e36f6f56295ee9ba58e48f04ee4c6e775e612ca
Marco Dominguez <47532404+MarcoDomi@users.noreply.github.com>
2024-12-02 20:39:35 -0600

    all locations as a random num of resources

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 
 const galaxyDimension = 80;
 let galaxy = document.querySelector("div");
+let galaxyResources = []; //list of resources at every location in galaxy
+let probe_list = [];
 
 function createRow() {
     let row = document.createElement("div");
     row.classList = "row";
 
+    //create galaxy location divs with class item
     for (let i = 0; i < galaxyDimension; i++){
         let item = document.createElement("div");
         item.classList = "item";
         row.appendChild(item);
+
+        galaxyResources.push(Math.floor(Math.random() * 30)); //add a value to galaxyResources corresponding to current galaxy location  
     }
 
     return row;
@@ -21,20 +26,33 @@ function createGalaxy() {
     }
 }
 
+function getRandomColor() {
+  let letters = '0123456789ABCDEF';
+  let color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
 
-let probe_list = [];
 
-
-function createProbe() {
-    return {
+function createProbe(location) {
+    let probe = {
+        color: getRandomColor(),
+        location,
         resources: 0,
-        checkResources: function () {
-            if (this.resources === 10)
-                return createProbe();
-    
+        duplicateSelf: function () {
+            if (this.resources >= 10) {
+                this.resources -= 10;
+                return createProbe(this.location);
+            }
             return none;
         }
     }
+
+    probe_list.push(probe); 
+
+    return probe;
 }
 
 
@@ -43,15 +61,17 @@ let galaxyLocations = document.querySelectorAll('.item');
 
 function getRandomLocation() {
     let locationCount = galaxyDimension * galaxyDimension;
-    return Math.floor(Math.random(locationCount) * locationCount);
+    return Math.floor(Math.random() * locationCount);
 }
 
-function placeProbe() {
-    let location = getRandomLocation();
-    galaxyLocations[location].style.backgroundColor = 'red'; //change to random color
-}
 
-placeProbe();
 function startSim() {
+    let location = getRandomLocation();
+    galaxyLocations[location].style.backgroundColor = 'red'; 
+
+    p = createProbe(location);
+
 
 }
+
+startSim()
